f509d268438df07318725db67bb889974e6d1c74
dchu <danchu56@gmail.com>
2020-09-24 01:24:57 -0400

    Fixed Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,21 +8,6 @@
 
 Restaurant.destroy_all
 
-    t.string "name", null: false
-    t.text "description", null: false
-    t.text "address", null: false
-    t.integer "capacity", null: false
-    t.string "gmaps_place_id", null: false
-    t.string "neighborhood"
-    t.string "phone_num", null: false
-    t.string "cuisine"
-    t.string "price_range"
-    t.string "payment"
-    t.string "dining_style"
-    t.string "dress_code"
-    t.string "public_transit"
-    t.string "parking_details"
-
 r1 = Restaurant.create!(
     name: "Union Square Market",
     description: "Green Market",
