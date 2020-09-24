8fdd1fa2bc8ebfccac11b4ef3bd62a343dfc7e3f
dchu <danchu56@gmail.com>
2020-09-24 01:43:44 -0400

    Testing Merge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
 Restaurant.destroy_all
-
-
 
 r1 = Restaurant.create!(
     name: "Union Square Market",
